dd7df61fc37bd4621623875c318192263e3f3af3
KICK BUTTOWSKI <brightonkofu@outlook.com>
2020-07-16 17:40:52 +0200

    Initial Commit

---
+++ Dab/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dab.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
